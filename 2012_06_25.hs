6a097312fa60a6ec70a4e044e17a7178bc18f3d1
Ondrej Kupka <ondra.cap@gmail.com>
2012-09-13 18:47:59 +0200

    2012_06_25: Haskell 2

--- 2012_06_25.hs
+++ 2012_06_25.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 {- 
    Isomorphic trees
    ----------------
@@ -27,3 +29,32 @@ iso n (Node l v r) =
                    ls <- iso (n-m-1) l,
                    rs <- iso m r
   ]
+
+{-
+   N-ary tree is given as
+
+       data NTree a = NTree a [NTree a]
+   
+   In our case the type parameter is a pair of (k, v).
+
+   So, write a function that visits all the nodes, sorting the list of children
+   according to a compare function given. The signature is
+
+       compare :: k -> k -> Bool
+
+   Use it to compare keys (parameter k). It returns True when k1 < k2.
+-}
+
+
+data NTree a = NTree a [NTree a] deriving Show
+
+sortNT :: (Eq k ) => (k -> k -> Bool) -> NTree (k, v) -> NTree (k, v)
+sortNT cmpf (NTree (key, value) children) =
+  NTree (key, value) schildren
+  where
+    mycmp (NTree (k1, _) _) (NTree (k2, _) _)
+      | k1 == k2 = EQ
+      | cmpf k1 k2 = LT
+      | otherwise = GT
+    chch = map (sortNT cmpf) children
+    schildren = sortBy mycmp chch
