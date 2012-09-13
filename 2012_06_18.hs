c20f62a7d0bddc8dcd40c37941fb2e349c0fb93e
Ondrej Kupka <ondra.cap@gmail.com>
2012-09-14 01:01:31 +0200

    2012_06_18: Haskell 2

--- 2012_06_18.hs
+++ 2012_06_18.hs
@@ -2,7 +2,7 @@ import Data.List
 import Data.Tuple
 
 {-
-  Multisets Comparison
+  Multiset Comparison
 
   Write a function for comparing multisets. A multiset is [(Int, a)], while
   (i, k) means that k is repeater i-times in the set.
@@ -22,3 +22,26 @@ cmpm m1 m2
     let m1f = filter (\e -> not $ elem e m2) m1
         m2f = filter (\e -> not $ elem e m1) m2
     in compare (swap $ last $ sort m1f) (swap $ last $ sort m2f)
+
+{-
+  Convert N-ary tree to binary tree
+
+  Write a function that converts a N-ary tree, that is
+
+      data NTree a = NTree a [NTree a]
+
+  The output data structure is
+
+      data BTree a = Void | BTree (BTree a) a (BTree a)
+-}
+
+data NTree a = NTree a [NTree a] deriving Show
+data BTree a = Void | BTree (BTree a) a (BTree a) deriving Show
+
+ntob :: NTree a -> BTree a
+ntob (NTree v []) = BTree Void v Void
+ntob (NTree v (c:cs)) =
+  let l = ntob c
+      r = ntob dummy
+  in  BTree l v r
+  where dummy = NTree v cs
