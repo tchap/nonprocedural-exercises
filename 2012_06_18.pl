506945028be90bf62fabf7dbe50438a5a5b58d57
Ondrej Kupka <ondra.cap@gmail.com>
2012-09-13 23:35:01 +0200

    2012_06_18: Prolog 2

--- 2012_06_18.pl
+++ 2012_06_18.pl
@@ -32,3 +32,49 @@ equiv_core(['*'], _).
 equiv_core(_, ['*']).
 equiv_core(['*'|_], L2) :- last(L2, '*').
 equiv_core(L1, ['*'|_]) :- last(L1, '*').
+
+/*
+ * You are given a list showing you which elements are comparable while a->b
+ * means a < b. Find all pairs of elements that are not comparable,
+ * e.g. there is no path from one element to the other.
+ *
+ * uncomp(+G, -UC)
+ */ 
+
+:- op(500, xfx, '-').
+:- op(500, xfx, '->').
+
+incomparable(Es, UCs) :-
+	vertices(Es, [], Vs),
+	findall_incomparable(Es, Vs, UCs).
+
+vertices([], Acc, UAcc) :- unique(Acc, [], UAcc).
+vertices([X->Y|Es], Acc, Vs) :-
+	vertices(Es, [X,Y|Acc], Vs).
+
+unique([], Acc, Acc).
+unique([X|Xs], Acc, Uniq) :-
+	member(X, Acc),
+	!,
+	unique(Xs, Acc, Uniq).
+unique([X|Xs], Acc, Uniq) :-
+	unique(Xs, [X|Acc], Uniq).
+
+findall_incomparable(Es, Vs, Ps) :- findall_icmp(Es, Vs, [], Ps).
+
+findall_icmp(Es, Vs, Acc, Ps) :-
+	member(X, Vs),
+	member(Y, Vs),
+	X \= Y,
+	\+ member(X-Y, Acc),
+	\+ member(Y-X, Acc),
+	\+ comparable(Es, X, Y),
+	\+ comparable(Es, Y, X),
+	findall_icmp(Es, Vs, [X-Y|Acc], Ps),
+	!.
+findall_icmp(_, _, Acc, Acc).
+
+comparable(Es, X, Z) :- member(X->Z, Es).
+comparable(Es, X, Z) :- 
+	member(X->Y, Es),
+	comparable(Es, Y, Z).
