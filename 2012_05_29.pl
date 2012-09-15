8af6266d30604cdb8e5b896b32a4a3811f900eca
Ondrej Kupka <ondra.cap@gmail.com>
2012-09-15 18:15:13 +0200

    2012_05_29: Prolog 2

--- 2012_05_29.pl
+++ 2012_05_29.pl
@@ -21,3 +21,60 @@ diffInMin(N, [Var-Val|Vs], [Var-NVal|DVs]) :-
 
 switch(0, 1).
 switch(1, 0).
+
+/*
+ * Given a directed graph without circles and multiedges and a set of vertices,
+ * modify the graph in a way that all the vertices in the set are colapsed into
+ * a single new vertex. Make sure that there are again no circles and multiedges.
+ */
+
+:- op(400, xfx, '->').
+
+colapse(G, Vs, NewG) :-
+	rename(G, Vs, colapsed, NewG),
+	\+ path(NewG, colapsed, colapsed).
+
+rename(G, Vs, NewV, [NewV->Out|TempG]) :-
+	rename(G, Vs, NewV, [], TempOut, [], TempG),
+	filter(TempOut, Vs, [], Out).
+
+rename([], _, _, OAcc, OAcc, GAcc, GAcc).
+rename([V->As|Es], Vs, NewV, OAcc, Out, GAcc, G) :-
+	member(V, Vs),
+	append(As, OAcc, NewOAcc),
+	rename(Es, Vs, NewV, NewOAcc, Out, GAcc, G).
+rename([V->As|Es], Vs, NewV, OAcc, Out, GAcc, G) :-
+	\+ member(V, Vs),
+	rename_list(As, Vs, NewV, [], NewAs),
+	rename(Es, Vs, NewV, OAcc, Out, [V->NewAs|GAcc], G).
+
+filter([], _, Acc, Acc).
+filter([V|Vs], Set, Acc, NewL) :-
+	(member(V, Set); member(V, Acc)),
+	!,
+	filter(Vs, Set, Acc, NewL).
+filter([V|Vs], Set, Acc, NewL) :-
+	filter(Vs, Set, [V|Acc], NewL).
+
+rename_list([], _, _, Acc, Acc).
+rename_list([V|Vs], CVs, NewV, Acc, NewL) :-
+	\+ member(V, CVs),
+	!,
+	rename_list(Vs, CVs, NewV, [V|Acc], NewL).
+rename_list([_|Vs], CVs, NewV, Acc, NewL) :-
+	\+ member(NewV, Acc),
+	!,
+	rename_list(Vs, CVs, NewV, [NewV|Acc], NewL).
+rename_list([_|Vs], CVs, NewV, Acc, NewL) :-
+	rename_list(Vs, CVs, NewV, Acc, NewL).
+
+path(G, V1, V2) :- edge(G, V1, V2).
+path(G, V1, V3) :-
+	edge(G, V1, V2),
+	path(G, V2, V3).
+
+edge([V1->Adjs|_], V1, V2) :-
+	member(V2, Adjs),
+	!.
+edge([_->_|Vs], V1, V2) :-
+	edge(Vs, V1, V2).
