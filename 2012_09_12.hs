77731c38ea4ff4ff42b773bdccd98ed20b57b124
Ondrej Kupka <ondra.cap@gmail.com>
2012-09-16 16:37:41 +0200

    2012_09_12: Haskell 2

--- 2012_09_12.hs
+++ 2012_09_12.hs
@@ -20,3 +20,25 @@ perm list constr = filter ff (allPerms list)
 allPerms :: Eq a => [a] -> [[a]]
 allPerms [] = [[]]
 allPerms list = [(p:ps) | p <- list, ps <- allPerms $ delete p list]
+
+{-
+  Write fold function for binary trees. The types are
+  
+      data BTree = Void | Node (BTree a) a (BTree a)
+      fold :: (b -> a -> b -> b) -> b -> BTree a -> b
+
+  Use that fold function to get the height of the tree and
+  count the number of leaves in the tree.
+-}
+
+data BTree = Void | Node (BTree a) a (BTree a)
+
+fold :: (b -> a -> b -> b) -> b -> BTree a -> b
+fold f acc Void = acc
+fold f acc (Node l v r) = f (fold f acc l) v (fold f acc r)
+
+height :: BTree a -> Int
+height = fold (\l _ r -> (+) 1 $ max l r) 0
+
+leaveCount :: BTree a -> [a]
+leaveCount = fold (\l _ r -> if (max l r) == 0 then 1 else l + r) 0
