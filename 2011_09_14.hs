340d682d6c7bfcca199d8e9c0fdee1b01b60389b
Ondrej Kupka <ondra.cap@gmail.com>
2012-09-16 22:09:05 +0200

    2011_09_14: Haskell 2

--- 2011_09_14.hs
+++ 2011_09_14.hs
@@ -11,3 +11,21 @@ indexOf perm = fromJust (elemIndex perm $ genPerms (sort perm))
 genPerms :: Eq a => [a] -> [[a]]
 genPerms [] = [[]]
 genPerms sorted = [(x:xs) | x <- sorted, xs <- genPerms (delete x sorted)]
+
+{-
+  Given a relation defined by a list of pairs of elements, add pairs to this list
+  so that the relation is equivalence (reflexive, transitive, symmetric)
+-}
+
+toEquiv :: Eq a => [(a, a)] -> [(a, a)]
+toEquiv = closureT . closureRS
+  where closureRS r = nub . concat $ map (\(a,b) -> [(a,a),(b,b),(a,b),(b,a)]) r
+        closureT r
+	  | length r == length closure = closure
+	  | otherwise = closureT closure
+	  where closure = nub $ [(a,d) | (a,b) <- r,
+	                                 (c,d) <- r,
+					 b == c
+				]
+				++
+				r
