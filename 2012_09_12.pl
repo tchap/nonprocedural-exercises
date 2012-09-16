a8652c630e9ddb868b0b87bfa0909915d6ccf375
Ondrej Kupka <ondra.cap@gmail.com>
2012-09-16 15:52:21 +0200

    2012_09_12: Prolog 2

--- 2012_09_12.pl
+++ 2012_09_12.pl
@@ -45,3 +45,36 @@ degree([_-V|Es], V, D) :-
 	!.
 degree([_|Es], V, D) :-
 	degree(Es, V, D).
+
+/*
+ * Given a list of rectangles and a window, which is yet another rectangle,
+ * modify all the rectangles in the list in a way that it shows what parts
+ * will be visible in the window.
+ *
+ * Example: crop(o(10, 30, 30, 10), [o(1, 20, 20, 1)], C).
+ *          -> C = [o(10, 20, 20, 10)].
+ *
+ * crop(+Window, +Rectangles, -VisibleParts)
+ */
+
+crop(_, [], []) :- !.
+crop(Window, [R|Rectangles], [Intersect|Cropped]) :-
+	intersect(Window, R, Intersect),
+	Intersect \= void,
+	!,
+	crop(Window, Rectangles, Cropped).
+crop(Window, [_|Rectangles], Cropped) :-
+	crop(Window, Rectangles, Cropped).
+
+intersect(o(WTLX, WTLY, WBRX, WBRY), o(RTLX, RTLY, RBRX, RBRY), o(RTLX, RTLY, RBRX, RBRY)) :-
+	WTLX =< RTLX, WTLY >= RTLY, WBRX >= RBRX, WBRY =< RBRY,
+	!.
+intersect(o(WTLX, WTLY, WBRX, WBRY), o(RTLX, RTLY, RBRX, RBRY), void) :-
+	(RBRY > WTLY; RTLY < WBRY; RBRX < WTLX; RTLX > WBRX),
+	!.
+intersect(o(WTLX, WTLY, WBRX, WBRY), o(RTLX, RTLY, RBRX, RBRY), o(MTLX, MTLY, MBRX, MBRY)) :-
+	MTLX is max(WTLX, RTLX),
+	MTLY is min(WTLY, RTLY),
+	MBRX is min(WBRX, RBRX),
+	MBRY is max(WBRY, RBRY).
+
