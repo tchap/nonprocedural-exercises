3cd52917e9012ea2b47292a0d03daf790e909029
Ondrej Kupka <ondra.cap@gmail.com>
2012-09-16 21:09:23 +0200

    2011_09_14: Prolog 2

--- 2011_09_14.pl
+++ 2011_09_14.pl
@@ -16,3 +16,19 @@ bracketing(N, D, Acc, Brackets) :-
 	D >= 0, N >= 0,
 	Dn is D - 1,
 	bracketing(N, Dn, ['('|Acc], Brackets).	
+
+/*
+ * Write conversion function between general tree (value + list of children)
+ * and canonical tree (list of children is converted into a new node,
+ * holding value, sibling and child). It should work in both directions.
+ * (http://ksvi.mff.cuni.cz/~topfer/ppt/Progr-7.pdf page 27)
+ *
+ * g2c(?GeneralTree, ?CanonicalTree)
+ */
+
+g2c(GT, CT) :- l2c([GT], CT).
+
+l2c([], nil).
+l2c([gt(V, Children)|GTs], ct(V, Child, Sibling)) :-
+	l2c(Children, Child),
+	l2c(GTs, Sibling).
