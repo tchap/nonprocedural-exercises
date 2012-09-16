3e54f328090530f297729d396b291873474aba38
Ondrej Kupka <ondra.cap@gmail.com>
2012-09-16 12:23:32 +0200

    2012_06_07: Haskell 2

--- 2012_06_07.hs
+++ 2012_06_07.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 {-
   Construct a binary search tree from a list of nodes
   that represent the preorder traversal of the tree.
@@ -11,3 +13,19 @@ readBST (x:xs) = Node l x r
   where (smaller, larger) = span (<x) xs
         l = readBST smaller
 	r = readBST larger
+
+{-
+  A graph is represented either as a list of edges, that is [(a, a, Int)]
+  or as a list of pairs (vertex, [edges]), that is [(a, [(a, Int)])].
+  Your task is to write a function that convert the former representation
+  into the latter one.
+-}
+
+type EdgGraph a = [(a, a, Int)]
+type AdjGraph a = [(a, [(a, Int)])]
+
+edg2adj :: Eq a => EdgGraph a -> AdjGraph a
+edg2adj g = map transf grps
+  where grps = groupBy (\(x, _, _) (y, _, _) -> x == y) g
+        transf grp@((v, _, _):_) = (v, es)
+	  where es = map (\(_, t, w) -> (t, w)) grp
