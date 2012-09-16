20c8b0ea58519c4ad9abf20009c095b77f3794b0
Ondrej Kupka <ondra.cap@gmail.com>
2012-09-16 11:17:20 +0200

    2012_06_07: Prolog 2

--- 2012_06_07.pl
+++ 2012_06_07.pl
@@ -17,3 +17,31 @@ limitTree(t(L, V, _), From, To, NewL) :-
 limitTree(t(L, V, R), From, To, t(NewL, V, NewR)) :-
 	limitTree(L, From, To, NewL),
 	limitTree(R, From, To, NewR).
+
+/*
+ * Given a list of vectors (which are in turn list of integers),
+ * find all vectors that are not dominated by any other.
+ * A vector v1 is dominated by v2 iff v1_i =< v2_i for all 0 < i =< len(v1).
+ */
+
+paretoMax(Vs, OutVs) :-
+	paretoMax(Vs, Vs, OutVs).
+
+paretoMax(_, [], []) :- !.
+paretoMax(All, [V|Vs], [V|OutVs]) :-
+	select(V, All, Rest),
+	check(V, Rest),
+	!,
+	paretoMax(All, Vs, OutVs).
+paretoMax(All, [_|Vs], OutVs) :-
+	paretoMax(All, Vs, OutVs).
+
+check(_, []).
+check(V, [O|Others]) :-
+	\+ dominatedBy(V, O),
+	check(V, Others).
+
+dominatedBy([], []).
+dominatedBy([V1|V1s], [V2|V2s]) :-
+	V1 =< V2,
+	dominatedBy(V1s, V2s).
